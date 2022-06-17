7c069aad700e42f733093cb523c9dad730932d82
guyi02 <guyi__@hotmail.com>
2022-06-17 14:08:11 -0300

    fix: husky lint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Home from 'pages/Home';
-const notusedVar = '';
+
 function App() {
-	return (
-		<ChakraProvider>
-			<Home />
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider>
+      <Home />
+    </ChakraProvider>
+  );
 }
 
 export default App;
